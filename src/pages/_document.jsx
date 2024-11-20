0cf68a92a432afc185a09659f38934f66d50616f
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-11-20 16:20:11 -0500

    color scheme changed

---
+++ src/pages/_document.jsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body id="root" className="bg-background text-zinc-300">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
